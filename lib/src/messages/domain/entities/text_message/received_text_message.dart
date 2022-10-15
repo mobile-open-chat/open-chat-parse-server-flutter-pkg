ae24df32e89d1a0f266ab8fbd9ebd726f80d9f02
nidal bakir <nedalbaker2000@gmail.com>
2022-10-15 15:23:40 +0300

    Create text message entities

---
+++ lib/src/messages/domain/entities/text_message/received_text_message.dart
@@ -0,0 +1,32 @@
+
+import '../received_message_base.dart';
+
+class ReceivedTextMessage extends ReceivedMessageBase {
+  
+  final String textMessage;
+
+  const ReceivedTextMessage({
+    required super.localMessageId,
+    required super.remoteMessageId,
+    required super.senderId,
+    required super.receiverId,
+    required super.localSentDate,
+    required super.localReceivedDate,
+    required super.remoteSentDate,
+    required super.messageDeliveryState,
+    required this.textMessage,
+  });
+
+  @override
+  List<Object?> get props => [
+        localMessageId,
+        remoteMessageId,
+        senderId,
+        receiverId,
+        localSentDate,
+        localReceivedDate,
+        remoteSentDate,
+        messageDeliveryState,
+        textMessage
+      ];
+}
---
+++ lib/src/messages/domain/entities/text_message/sent_text_message.dart
@@ -0,0 +1,32 @@
+
+import '../sent_message_base.dart';
+
+class SentTextMessage extends SentMessageBase {
+
+  final String textMessage;
+
+  const SentTextMessage({
+    required super.localMessageId,
+    required super.remoteMessageId,
+    required super.senderId,
+    required super.receiverId,
+    required super.localSentDate,
+    required super.localReceivedDate,
+    required super.remoteSentDate,
+    required super.messageDeliveryState,
+    required this.textMessage,
+  });
+
+  @override
+  List<Object?> get props => [
+        localMessageId,
+        remoteMessageId,
+        senderId,
+        receiverId,
+        localSentDate,
+        localReceivedDate,
+        remoteSentDate,
+        messageDeliveryState,
+        textMessage
+      ];
+}

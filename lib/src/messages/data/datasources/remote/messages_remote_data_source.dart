8cb0343311af382a98fc20025b91c711ec80dc67
nidal bakir <nedalbaker2000@gmail.com>
2022-10-27 08:08:34 +0300

    feat(remote): 🚧 add "downloadFile" function to remote data source

--- lib/src/messages/data/datasources/remote/messages_remote_data_source.dart
+++ lib/src/messages/data/datasources/remote/messages_remote_data_source.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
+import 'package:path/path.dart' as path;
 
 import '../../../../core/error/exceptions/server_exception.dart';
 import '../../../../core/error/exceptions/user_exception.dart';
@@ -11,6 +14,11 @@ abstract class MessagesRemoteDataSource {
 
   Future<void> uploadImageMessage(
     RemoteMessageModel message,
+    void Function(int progress, int total) progressCallback,
+  );
+
+  Future<File> downloadFile(
+    String url,
     void Function(int progress, int total) progressCallback,
   );
 
@@ -81,6 +89,30 @@ class MessagesRemoteDataSourceImpl extends MessagesRemoteDataSource {
       return sendMessageResponse.results!.first as RemoteMessageModel;
     } else {
       throw ParseException.extractParseException(sendMessageResponse.error);
+    }
+  }
+
+  @override
+  Future<File> downloadFile(
+    String url,
+    void Function(int progress, int total) progressCallback,
+  ) async {
+    var parseFile = ParseFile(
+      null,
+      url: url,
+      name: path.basename(url),
+    );
+
+    try {
+      parseFile = await parseFile.download(
+        progressCallback: progressCallback,
+      );
+
+      return parseFile.file!;
+    } catch (error) {
+      throw InternetConnectionException(
+        "Can't download the file connection error \nError:$error",
+      );
     }
   }
 }

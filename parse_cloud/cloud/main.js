554e39d022ab2d7c871eac4d4f8de49e9cb26198
nidal bakir <nedalbaker2000@gmail.com>
2022-11-09 15:43:42 +0300

    feat(cloudFuc): 🚧 implements beforeSave for messages table in the database

--- parse_cloud/cloud/main.js
+++ parse_cloud/cloud/main.js
@@ -88,3 +88,96 @@ function isMessageSentByThisUser(userID, message) {
 function isReceivedMessageDeliveryStateIsSeen(message) {
   return message.get("messageDeliveryState") === "seen";
 }
+
+Parse.Cloud.beforeSave("Messages", async (request) => {
+  let message = request.object;
+  if (message.isNew()) {
+    message.set("sender", request.user.toPointer());
+    await checkIfTheSenderWasBlockedByReceiver(message);
+    setChatIDForMessage(message);
+    setMessageACL(message);
+
+    if (message.get("messageType") === "image") {
+      await createThumbnail(message);
+    }
+  }
+});
+
+/**
+ * @param {Parse.Object<Parse.Attributes>} message
+ */
+function setChatIDForMessage(message) {
+  const senderId = message.get("sender").id;
+  const receiverId = message.get("receiver").id;
+  const chatID = generateChatID(senderId, receiverId);
+
+  message.set("chatID", chatID);
+}
+
+/**
+ * @param {String} ID1 first id
+ * @param {String} ID2 second id
+ */
+function generateChatID(ID1, ID2) {
+  let chatID;
+  if (ID1 > ID2) {
+    chatID = ID1.concat(ID2);
+  } else {
+    chatID = ID2.concat(ID1);
+  }
+
+  return chatID;
+}
+
+async function checkIfTheSenderWasBlockedByReceiver(message) {
+  const senderId = message.get("sender").id;
+  const receiver = await message.get("receiver").fetch({ useMasterKey: true });
+
+  let receiverBlockedUsersList = receiver.get("blockedUsersArray");
+  if (receiverBlockedUsersList.includes(senderId)) {
+    throw new Parse.Error(
+      1000,
+      "the sender user was blocked by the receiver user, can not send a message"
+    );
+  }
+}
+
+/**
+ * @param {Parse.Object<Parse.Attributes>} message
+ */
+function setMessageACL(message) {
+  const sender = message.get("sender");
+  const receiver = message.get("receiver");
+
+  let acl = new Parse.ACL();
+  acl.setReadAccess(sender, true);
+  acl.setWriteAccess(sender, true);
+
+  acl.setReadAccess(receiver, true);
+  acl.setWriteAccess(receiver, true);
+
+  message.setACL(acl);
+}
+
+/**
+ * @param {Parse.Object<Parse.Attributes>} message
+ */
+async function createThumbnail(message) {
+  try {
+    const image = message.get("file");
+    const imageDate = await image.getData();
+
+    const imageFileBuffer = Buffer.from(imageDate, "base64");
+
+    const options = { percentage: 5, responseType: "base64" };
+    const thumbnail = await imageThumbnail(imageFileBuffer, options);
+
+    const thumbFile = new Parse.File(image.name(), { base64: thumbnail });
+
+    const imageThumbnailFile = await thumbFile.save();
+
+    message.set("thumbnail", imageThumbnailFile);
+  } catch (error) {
+    console.log("error while generating image thumbnail error:" + error);
+  }
+}
